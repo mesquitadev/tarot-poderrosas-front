dafb1c986dbf281e92182e82ab4037086a18e71b
Victor Mesquita <mesquitadev@gmail.com>
2023-07-10 21:25:35 -0300

    feat: :tada: initialize project

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), VitePWA({ registerType: 'autoUpdate' })],
+  plugins: [react(), tsconfigPaths()],
   server: {
     watch: {
       usePolling: true,
